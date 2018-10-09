09dc3e4914e33f82ac45cdfe310dbd50fa9f1dc7
Himenon <k.himeno314@gmail.com>
2018-10-10 00:38:26 +0900

    update(ts): tsconfigの調整

--- sample/get-title-ts/src/__tests__/main.test.ts
+++ sample/get-title-ts/src/__tests__/main.test.ts
@@ -3,9 +3,7 @@ import { getTitle } from "../main";
 describe("getSiteTitleのテスト", () => {
   test("https://github.com/", async () => {
     const title = await getTitle("https://github.com/");
-    expect(title).toEqual(
-      "The world’s leading software development platform · GitHub"
-    );
+    expect(title).toEqual("The world’s leading software development platform · GitHub");
   });
 
   test("http://example.com/", async () => {
